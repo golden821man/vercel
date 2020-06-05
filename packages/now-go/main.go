golden821man/vercel

commit 56c8af51b23aa8f4a457743a0762b303361d5cfe

[all] Rename GitHub repo to vercel/vercel (#4506)
We renamed the GitHub repository from `zeit/now` to `vercel/vercel` so this PR updates all references to the repo URL.

There were also a few remaining references to Now CLI that have been updated to Vercel CLI.

packages/now-go/main.go
@@ -2,9 +2,9 @@ package main
 
 import (
 	"net/http"
-	now "github.com/zeit/now-go-bridge/go/bridge"
+	vc "github.com/vercel/go-bridge/go/bridgee"
 )
 
 func main() {
-	now.Start(http.HandlerFunc(__NOW_HANDLER_FUNC_NAME))
+	vc.Start(http.HandlerFunc(__NOW_HANDLER_FUNC_NAME))
 }

commit 284879f4fcc78fd88d246f530e1ef016ef6f4a20

[go] Fix typo in `go-bridge` import (#4578)
Introduced in 56c8af51b.

packages/now-go/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"net/http"
-	vc "github.com/vercel/go-bridge/go/bridgee"
+	vc "github.com/vercel/go-bridge/go/bridge"
 )
 
 func main() {